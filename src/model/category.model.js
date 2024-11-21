8490e70900542e3e896187cca285527eb63eb079
sajdkabir <sajda.kbir@gmail.com>
2024-11-22 00:39:58 +0530

    added category

---
+++ src/model/category.model.js
@@ -0,0 +1,41 @@
+import { Schema } from "mongoose";
+import { v4 as uuid } from "uuid";
+import { db } from "../../loaders/db.loader.js";
+import { number } from "joi";
+
+const CategorySchema = new Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+}, {
+    timestamps: true
+});
+
+CategorySchema.index({
+    name: "text"
+})
+
+CategorySchema.pre('save', async function(next) {
+    if (this.isNew) {
+        const lastCategory = await this.constructor.findOne().sort({ id: -1 });
+        this.id = lastCategory ? lastCategory.id + 1 : 1;
+    }
+    next();
+});
+
+const Category = db.model('Category', CategorySchema, 'categories')
+
+export {
+    Category
+};
