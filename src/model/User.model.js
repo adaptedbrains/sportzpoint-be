20d8342fa2538b7d48d2414da7409f6b8f49bafa
sajdkabir <sajda.kbir@gmail.com>
2024-11-22 11:13:29 +0530

    user model added

--- src/model/User.model.js
+++ src/model/User.model.js
@@ -1,26 +1,27 @@
 import mongoose from "mongoose";
 import { db } from "../../loaders/db.loader.js";
 
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    meta_title: { type: String },
+    meta_description: { type: String },
+    display_order: { type: Number, required: true },
+    roles: {
+        type: Schema.Types.Array
+    },
+    description: { type: String },
+    hide_on_website: { type: Boolean, default: false },
+    social_profiles: [{ type: String }],
+    profile_picture: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  slug: { type: String, required: true, unique: true },
-  meta_title: { type: String },
-  meta_description: { type: String },
-  display_order: { type: Number, required: true },
-  role: { type: String, required: true },
-  description: { type: String },
-  hide_on_website: { type: Boolean, default: false },
-  social_profiles: [{ type: String }],
-  profile_picture: { type: String }, 
-}, {
-  timestamps: true
-});
+const User = db.model("User", UserSchema, "users");
 
-
-const User = db.model('User', UserSchema, 'users')
-
-export {
-    User
-};
+export { User };
