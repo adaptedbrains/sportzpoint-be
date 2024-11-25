08a6bc733e974f826a7808ac90e586743bc7ba37
sajdkabir <sajda.kbir@gmail.com>
2024-11-25 16:10:15 +0530

    added middleware

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { publishPostController , getPendingApprovalPostsController, unpublishPostController} from '../controllers/admin.controller.js';
-import { isAdmin, authenticateJWT } from '../middleware/auth.middleware.js';
+import { isAdmin, authenticateJWT, checkRole } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.route("/post/publish/:id").get(authenticateJWT, isAdmin, publishPostController);
-router.route("/post/unpublish/:id").patch(authenticateJWT, isAdmin, unpublishPostController);
-router.get("/posts/pending-approval", authenticateJWT, isAdmin, getPendingApprovalPostsController);
+router.route("/post/publish/:id").get(authenticateJWT, checkRole(['Admin', 'Editor']), publishPostController);
+
+router.route("/post/unpublish/:id").patch(authenticateJWT, checkRole(['Admin', 'Editor']), unpublishPostController);
+
+router.get("/posts/pending-approval", authenticateJWT, checkRole(['Admin, Editor']), getPendingApprovalPostsController);
 
 export default router;
