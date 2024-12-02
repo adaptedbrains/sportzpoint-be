02cf6723dc9163673f7520052634895f7968f08b
sajdkabir <sajda.kbir@gmail.com>
2024-12-02 22:52:34 +0530

    fix dleete cat

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -77,14 +77,11 @@ export const updateCategory = async (req, res) => {
 export const deleteCategory = async (req, res) => {
     try {
         const { id } = req.params;
+        console.log("dkf: ", id);
 
-        const category = await Category.findOne({_id: id });
+        const category = await Category.findByIdAndDelete({_id: id });
 
-        if (!category) {
-            return res.status(404).json({ message: 'Category not found' });
-        }
 
-        await category.remove();
 
         return res.status(200).json({ message: 'Category deleted successfully' });
     } catch (error) {
